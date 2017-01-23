4ec2fb174d7735244b99140a5a37dc6fa819fe56
415000 <camendoza@DIRECTV.com>
2017-01-22 17:15:28 -0800

    Added wikipedia links to description

--- js/script.js
+++ js/script.js
@@ -5,63 +5,76 @@ var infoWindow;
 var infoWindowMsg;
 var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
+// var locations = [
+//     {lat: 33.92, lng: -118.39,name:"El Segundo",description:"El Segundo is a city in Los Angeles County, California, United States. El Segundo, from Spanish, means The Second in English."},
+//     {lat: 34.92, lng: -118.39,name:"Twin Lakes",description:"Fishing - What the Eastern Sierra's are known for! Please read your reg's before fishing, as they change each year."},
+//     {lat: 35.92, lng: -118.39,name:"Sequoia National Forest",description:"Sequoia National Forest is located in the southern Sierra Nevada mountains of California. The U.S. National Forest is named for the majestic Giant Sequoia trees which populate 38 distinct groves within the boundaries of the forest"},
+//     {lat: 36.92, lng: -118.39,name:"Kings Canyon National Park",description:"Kings Canyon National Park is adjacent to Sequoia National Park in California's Sierra Nevada mountains. It's known for its huge sequoia trees, notably the gigantic General Grant Tree in Grant Grove. To the east, Cedar Grove is surrounded by towering granite canyon walls. From here, trails lead to Zumwalt Meadow along the Kings River, and to Roaring River Falls. The park is home to rattlesnakes, bears and cougars."},
+//     {lat: 37.92, lng: -118.39,name:"White Mountain Peak",description:"White Mountain Peak, at 14,252-foot, is the highest peak in the White Mountains of California, the highest peak in Mono County, and the third highest peak in the state after Mount Whitney and Mount Williamson."},
+//     {lat: 38.92, lng: -118.39,name:"Walker River Reservation",description:"The Walker River Indian Reservation is an Indian reservation located in central Nevada in the United States. It belongs to the Walker River Paiute Tribe, a federally recognized tribe of Northern Paiute people."},
+//     {lat: 39.92, lng: -118.39,name:"Humboldt State Wildlife Management Area",description:"The Humboldt Wildlife Management Area is a wildlife management area in the U.S. state of Nevada, encompassing the salt marshes at the terminus of the Humboldt River"},
+//     {lat: 32.92, lng: -118.39,name:"San Clemente Island",description:"San Clemente Island is the southernmost of the Channel Islands of California. It is owned and operated by the United States Navy, and is a part of Los Angeles County"},
+//     {lat: 31.92, lng: -110.39,name:"Apache Peak",description:"Apache Peak Trail is a 9.7 mile moderately trafficked out and back trail located near Mountain Center, California that features beautiful wild flowers and is rated as difficult. The trail offers a number of activity options and is accessible year-round. Dogs and horses are also able to use this trail."},
+//     {lat: 30.92, lng: -110.39,name:"Cananea",description:"Cananea is a city in the northern Mexican state of Sonora, Northwestern Mexico. It is the seat of the Municipality of Cananea, on the U.S−Mexico border. The population of the city was 31,560 as recorded by the 2010 census."},
+//     {lat: 40.92, lng: -110.39,name:"Uinta-Wasatch-Cache National Forest",description:"Wasatch-Cache National Forest is a United States National Forest located primarily in northern Utah, with smaller parts extending into southeastern Idaho and southwestern Wyoming."},
+//     {lat: 41.92, lng: -110.39,name:"Kemmerer",description:"Kemmerer is both the largest city and the county seat of Lincoln County, Wyoming, United States. The population was 2,656 at the 2010 census. As the county seat of Lincoln County, Kemmerer is the location of the Lincoln County Courthouse."}
+//     ]
+
 var locations = [
-    {lat: 33.92, lng: -118.39,name:"El Segundo",description:"El Segundo is a city in Los Angeles County, California, United States. El Segundo, from Spanish, means The Second in English."},
-    {lat: 34.92, lng: -118.39,name:"Twin Lakes",description:"Fishing - What the Eastern Sierra's are known for! Please read your reg's before fishing, as they change each year."},
-    {lat: 35.92, lng: -118.39,name:"Sequoia National Forest",description:"Sequoia National Forest is located in the southern Sierra Nevada mountains of California. The U.S. National Forest is named for the majestic Giant Sequoia trees which populate 38 distinct groves within the boundaries of the forest"},
-    {lat: 36.92, lng: -118.39,name:"Kings Canyon National Park",description:"Kings Canyon National Park is adjacent to Sequoia National Park in California's Sierra Nevada mountains. It's known for its huge sequoia trees, notably the gigantic General Grant Tree in Grant Grove. To the east, Cedar Grove is surrounded by towering granite canyon walls. From here, trails lead to Zumwalt Meadow along the Kings River, and to Roaring River Falls. The park is home to rattlesnakes, bears and cougars."},
-    {lat: 37.92, lng: -118.39,name:"White Mountain Peak",description:"White Mountain Peak, at 14,252-foot, is the highest peak in the White Mountains of California, the highest peak in Mono County, and the third highest peak in the state after Mount Whitney and Mount Williamson."},
-    {lat: 38.92, lng: -118.39,name:"Walker River Reservation",description:"The Walker River Indian Reservation is an Indian reservation located in central Nevada in the United States. It belongs to the Walker River Paiute Tribe, a federally recognized tribe of Northern Paiute people."},
-    {lat: 39.92, lng: -118.39,name:"Humboldt State Wildlife Management Area",description:"The Humboldt Wildlife Management Area is a wildlife management area in the U.S. state of Nevada, encompassing the salt marshes at the terminus of the Humboldt River"},
-    {lat: 32.92, lng: -118.39,name:"San Clemente Island",description:"San Clemente Island is the southernmost of the Channel Islands of California. It is owned and operated by the United States Navy, and is a part of Los Angeles County"},
-    {lat: 31.92, lng: -110.39,name:"Apache Peak",description:"Apache Peak Trail is a 9.7 mile moderately trafficked out and back trail located near Mountain Center, California that features beautiful wild flowers and is rated as difficult. The trail offers a number of activity options and is accessible year-round. Dogs and horses are also able to use this trail."},
-    {lat: 30.92, lng: -110.39,name:"Cananea",description:"Cananea is a city in the northern Mexican state of Sonora, Northwestern Mexico. It is the seat of the Municipality of Cananea, on the U.S−Mexico border. The population of the city was 31,560 as recorded by the 2010 census."},
-    {lat: 40.92, lng: -110.39,name:"Uinta-Wasatch-Cache National Forest",description:"Wasatch-Cache National Forest is a United States National Forest located primarily in northern Utah, with smaller parts extending into southeastern Idaho and southwestern Wyoming."},
-    {lat: 41.92, lng: -110.39,name:"Kemmerer",description:"Kemmerer is both the largest city and the county seat of Lincoln County, Wyoming, United States. The population was 2,656 at the 2010 census. As the county seat of Lincoln County, Kemmerer is the location of the Lincoln County Courthouse."}
+    {lat: 33.92, lng: -118.39,name:"El Segundo",description:""},
+    {lat: 34.92, lng: -118.39,name:"Twin Lakes",description:""},
+    {lat: 35.92, lng: -118.39,name:"Sequoia National Forest",description:""},
+    {lat: 36.92, lng: -118.39,name:"Kings Canyon National Park",description:""},
+    {lat: 37.92, lng: -118.39,name:"White Mountain Peak",description:""},
+    {lat: 38.92, lng: -118.39,name:"Walker River Reservation",description:""},
+    {lat: 39.92, lng: -118.39,name:"Humboldt State Wildlife Management Area",description:""},
+    {lat: 32.92, lng: -118.39,name:"San Clemente Island",description:""},
+    {lat: 31.92, lng: -110.39,name:"Apache Peak",description:""},
+    {lat: 30.92, lng: -110.39,name:"Cananea",description:""},
+    {lat: 40.92, lng: -110.39,name:"Uinta-Wasatch-Cache National Forest",description:""},
+    {lat: 41.92, lng: -110.39,name:"Kemmerer",description:""}
     ]
 
 function loadData() {
 
-    var $body = $('body');
-    var $wikiElem = $('#wikipedia-links');
+
+    locations.forEach(function(location){
+        //self.resultList.push(new Location(location));
 
 
-    var $greeting = $('#greeting');
-    var $svImg = $('#svImg')
 
-    var $inputCity = $('#city')
+        url = "http://en.wikipedia.org/w/api.php"
+        url += '?' + $.param({
+          'action': 'opensearch',
+          'search': location.name,
+          'format':'json'
 
-    // clear out old data before new request
-    $wikiElem.text("");
+        });
+        var w_items = [];
+        var wikiRequestTimeout = setTimeout(function(){
+            $wikiElem.text("Failed do get descriptions from Wikipedia.")
+        },8000)
+        $.ajax({
+            'url': url,
+            'dataType':'jsonp',
+            'crossDomain':true,
+            'success':function(data) {
+                console.log(data)
+                // $.each( data[1], function( key, val ) {
+                //     w_items.push( "<li id='" + key + "'><a href='" + data[3][key] + "'>" + val + "</a></li>" );
+                // });
+                // var wikiItems = w_items.join("");
+                // $wikiElem.append(wikiItems);
 
-    var sCity=$inputCity[0].value,sAddress=$inputStreet[0].value;
+                location.description = data[2][1]
 
-    url = "http://en.wikipedia.org/w/api.php"
-    url += '?' + $.param({
-      'action': 'opensearch',
-      'search': sCity,
-      'format':'json'
+                clearTimeout(wikiRequestTimeout);
+            }
+        }).done(function(){
+            console.log('Done?')
+        })
 
-    });
-    var w_items = [];
-    var wikiRequestTimeout = setTimeout(function(){
-        $wikiElem.text("Failed do get wikipedia resources.")
-    },8000)
-    $.ajax({
-        'url': url,
-        'dataType':'jsonp',
-        'crossDomain':true,
-        'success':function(data) {
-            console.log(data)
-            $.each( data[1], function( key, val ) {
-                w_items.push( "<li id='" + key + "'><a href='" + data[3][key] + "'>" + val + "</a></li>" );
-            });
-            var wikiItems = w_items.join("");
-            $wikiElem.append(wikiItems);
-            clearTimeout(wikiRequestTimeout);
-        }
-    }).done(function(){
-        console.log('Done?')
+
     })
 
     return false;
@@ -180,7 +193,8 @@ var Location = function(data){
 
 var ViewModel = function() {
     var self = this;
-    this.map =
+
+    loadData();
     this.resultList = ko.observableArray([]);
     var url="https://maps.googleapis.com/maps/api/js?key=AIzaSyBFfd_W6FyD5M5YQVTwDKnP4mX4Tosj2Yw"
     this.txtFilter = ko.observable();
